8e7efb473ad9eb053d960b88a4a017872b6728bf
ChaimaeElKadiri <chaimaelkadiri4@gmail.com>
2024-03-19 12:14:26 +0100

    First Commit

---
+++ src/test/java/ma/chaimae/tp2app/Tp2AppApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.chaimae.tp2app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
